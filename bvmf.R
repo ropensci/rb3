fdef0cc6f5b4a68f586ad3e587d76e0a9ba68ca1
wilsonfreitas <wilson.freitas@gmail.com>
2015-04-01 09:28:09 -0300

    Código mais estruturado

--- bvmf.R
+++ bvmf.R
@@ -1,5 +1,8 @@
+library(httr)
+library(XML)
+library(functional)
 
-.get_curve_url <- function(refDate, item='PRE') {
+get_curve_url <- function(refDate, item='PRE') {
     url <- 'http://www2.bmf.com.br/pages/portal/bmfbovespa/boletim1/TxRef1.asp?Data=%s&Data1=%s&slcTaxa=%s'
     sprintf(url,
         format(as.Date(refDate), '%d/%m/%Y'),
@@ -7,26 +10,73 @@
         item)
 }
 
-get_curve <- function (refDate, item='PRE') {
+clean_spaces <- function(x) gsub('[\r\n \t]+', '', x)
+
+period2dot <- function(x) gsub(',', '.', x)
+
+extract_int <- function(x) sub("^(\\d+).*$", "\\1", x)
+
+clean_numeric <- Compose(
+  XML::xmlValue,
+  clean_spaces,
+  period2dot,
+  as.numeric
+)
+
+clean_integer <- Compose(
+  XML::xmlValue,
+  clean_spaces,
+  extract_int,
+  as.integer
+)
+
+
+clean_text <- Compose(
+  XML::xmlValue,
+  clean_spaces
+)
+
+
+download_data <- function(url, dest) {
+    req <- GET(url)
+    text <- content(req, as='text')
+    writeLines(text, dest)
+    NULL
+}
+
+
+parse_data <- function (file) {
+    doc <- XML::htmlTreeParse(file, useInternalNodes=TRUE)
+    itm <- XML::xpathSApply(doc, "//td[contains(@class, 'tabelaItem')]", clean_text)
+    num <- XML::xpathSApply(doc, "//td[contains(@class, 'tabelaConteudo')]", clean_numeric)
+    list(columns=length(itm) + 1, data=num)
+}
+
+
+get_curve_data <- function(refDate, item='PRE', dest=tempfile()) {
     refDate <- as.Date(refDate)
-    url <- .get_curve_url(refDate, item)
-    doc <- htmlTreeParse(url, useInternalNodes=TRUE)
+    url <- get_curve_url(refDate, item)
+    download_data(url, dest)
+    list(file=dest, refDate=refDate, item=item)
+}
 
-    tit <- xpathSApply(doc, "//td[contains(@class, 'tabelaTitulo')]", xmlValue)
 
-    num <- xpathSApply(doc, "//td[contains(@class, 'tabelaConteudo')]",
-        function(x) gsub('[\r\n \t]+', '', xmlValue(x)))
+get_curve <- function(curve_data) {
+    data <- parse_data(curve_data$file)
+    data <- matrix(data$data, nrow=data$columns)
+    list(curve=curve_data$item, terms=curve_data$refDate + data[1,] , rates=data[2,]/100)
+}
 
-    num <- sapply(num, function(x) {
-        as.numeric(gsub(',', '.', x))
-    }, USE.NAMES=FALSE)
 
-    terms <- bizdays(refDate, refDate + num[c(TRUE, FALSE, FALSE)])/252
-    rates <- num[c(FALSE, TRUE, FALSE)]/100
+gen_log_price_interpolator <- function(rates, terms) approxfun(terms, log((1 + rates)^terms), method='linear')
 
-    log_price_interpolator <- approxfun(terms, log((1 + rates)^terms), method='linear')
+
+gen_flat_forward_interpolator <- function(price_interp) {
     function (term) {
-        pu <- exp(log_price_interpolator(term))
+        pu <- exp(price_interp(term))
         pu^(1/term) - 1
     }
 }
+
+# log_price_interpolator <- gen_log_price_interpolator(rates, terms)
+# gen_flat_forward_interpolator(log_price_interpolator)
