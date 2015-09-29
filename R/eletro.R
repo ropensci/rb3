d60e81217a9f48d880dd63b74ae490ecbeb67f31
wilsonfreitas <wilson.freitas@gmail.com>
2015-09-29 09:03:33 -0300

    Novo template para swaps de balcão INCOMPLETO

---
+++ R/eletro.R
@@ -0,0 +1,42 @@
+
+Eletro <- MarketDataFWF$proto(expr={
+	filename <- 'Eletro.txt'
+	
+	parser <- textparser::textparser(
+		parse_sign=textparser::parser('^(\\+|-)$', function(text, match) {
+			idx <- text == '-'
+			x <- rep(1, length(text))
+			x[idx] <- -1
+			x
+		})
+	)
+	
+	fields <- fields(
+		field("Identificação da transação", width=6),
+		field("Complemento da transação", width=3),
+		field("Tipo de tegistro", width=2),
+		field("Data de geração do arquivo", width=8),
+		field("Tipo de negociação", width=2),
+		field("Código da mercadoria", width=3),
+		field("Código do mercado", width=1),
+		field("Data-base", width=8),
+		field("Data de vencimento", width=8),
+		field("Volume do dia em R$", width=13),
+		field("Volume do dia em US$", width=13),
+		field("Qtd. contratos em aberto após liquidação", width=8),
+		field("Qtd. de negócios efetuados", width=8),
+		field("Qtd. de contratos negociados", width=8),
+		field("Qtd. de contratos aberto antes da liquidação", width=8),
+		field("Qtd. de contratos liquidados", width=8),
+		field("Qtd. de contratos aberto final", width=8),
+		field("Taxa média (swp)/prêmio médio (opç flex)", width=9),
+		field("Sinal da taxa média/prêmio médio", width=1),
+		field("Tipo de taxa média", width=1),
+		field("Preço de exercício médio (opç flex)", width=22),
+		field("Sinal do preço médio de exercício", width=1),
+		field("Volume aberto em R$", width=13),
+		field("Volume aberto em US$", width=13)
+	)
+})
+
+MarketData$register(Eletro)
