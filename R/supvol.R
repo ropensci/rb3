5fc60123250a417f0f97aba0a0f994227ee0f30e
wilsonfreitas <wilson.freitas@gmail.com>
2015-09-29 08:24:48 -0300

    Novo template para arquivo de superfícies de volatilidade em delta para opções de balcão

---
+++ R/supvol.R
@@ -0,0 +1,28 @@
+
+SupVol <- MarketDataMultiPartCSV$proto(expr={
+	filename <- 'SupVol.txt'
+	separator <- ';'
+	
+	parts <- list(
+		'Cabeçalho'=list(
+			lines=1,
+			fields=fields(
+				field('Data de geração do arquivo', handler=to_date('%Y%m%d')),
+				field('Identificação do arquivo')
+			)
+		),
+		'Corpo'=list(
+			lines=-1,
+			fields=fields(
+				field('Código da Curva de Volatilidade'),
+				field('Descrição da Curva de Volatilidade'),
+				field('Prazo (dias de Saque)'),
+				field('Prazo (dias Corridos)'),
+				field('Valor', handler=to_numeric(dec=7))
+			)
+		)
+	)
+})
+
+MarketData$register(SupVol)
+
