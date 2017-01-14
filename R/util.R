537fffecb52d3f10c80ffc01d9b16d56241ec2a8
wilsonfreitas <wilson.freitas@gmail.com>
2017-01-14 12:01:55 -0200

    Changed tabs with spaces

--- R/util.R
+++ R/util.R
@@ -1,40 +1,40 @@
 
 
 read_fwf <- function(fname, widths, colnames=NULL, skip=0, text) {
-	colpositions <- list()
-	x <- 1
-	i <- 1
-	for (y in widths) {
-		colpositions[[i]] <- c(x, x+y-1)
-		x <- x + y
-		i <- i + 1
-	}
+  colpositions <- list()
+  x <- 1
+  i <- 1
+  for (y in widths) {
+    colpositions[[i]] <- c(x, x+y-1)
+    x <- x + y
+    i <- i + 1
+  }
 
-	if (is.null(colnames))
-		colnames <- paste0('V', seq_along(widths))
+  if (is.null(colnames))
+    colnames <- paste0('V', seq_along(widths))
 
-	lines <- if (missing(text)) readLines(fname) else text
+  lines <- if (missing(text)) readLines(fname) else text
 
-	if (skip)
-		lines <- lines[-seq(skip),]
+  if (skip)
+    lines <- lines[-seq(skip),]
 
-	t <- list()
-	for (i in seq_along(colnames)) {
-		dx <- colpositions[[i]]
-		t[[colnames[i]]] <- stringr::str_sub(lines, dx[1], dx[2])
-	}
+  t <- list()
+  for (i in seq_along(colnames)) {
+    dx <- colpositions[[i]]
+    t[[colnames[i]]] <- stringr::str_sub(lines, dx[1], dx[2])
+  }
 
-	as.data.frame(t, stringsAsFactors=FALSE, optional=TRUE, check.names=FALSE)
+  as.data.frame(t, stringsAsFactors=FALSE, optional=TRUE, check.names=FALSE)
 }
 
 
 trim_fields <- function(x) {
-	fields <- lapply(x, function(z) {
-		if (is(z, 'character'))
-			stringr::str_trim(z)
-		else
-			z
-	})
-	do.call('data.frame', c(fields, stringsAsFactors=FALSE, check.names=FALSE))
+  fields <- lapply(x, function(z) {
+    if (is(z, 'character'))
+      stringr::str_trim(z)
+    else
+      z
+  })
+  do.call('data.frame', c(fields, stringsAsFactors=FALSE, check.names=FALSE))
 }
 
