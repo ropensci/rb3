9639a793ed4443c2077862206ab6c9b3256a0ad8
wilsonfreitas <wilson.freitas@gmail.com>
2017-01-14 11:19:09 -0200

    Changes in handlers
    handlers have the attribute *type* to inform the user the destination type of the `field`.

--- R/handlers.R
+++ R/handlers.R
@@ -1,50 +1,61 @@
 
 width <- function(x) {
-	x <- as.numeric(x)
-	class(x) <- c('numeric', 'width')
-	x
+  x <- as.numeric(x)
+  class(x) <- c('numeric', 'width')
+  x
 }
 
 to_date <- function(format=NULL) {
-	if (is.null(format))
-		format <- '%Y-%m-%d'
-	handler <- function(x) {
-		as.Date(x, format=format)
-	}
-	class(handler) <- c('function', 'handler')
-	handler
+  if (is.null(format))
+    format <- '%Y-%m-%d'
+  handler <- function(x) {
+    as.Date(x, format=format)
+  }
+  attr(handler, 'type') <- 'Date'
+  class(handler) <- c('function', 'handler')
+  handler
 }
 
 to_time <- function(format=NULL) {
-	if (is.null(format))
-		format <- '%H:%M:%S'
-	handler <- function(x) {
-		strptime(x, format=format)
-	}
-	class(handler) <- c('function', 'handler')
-	handler
+  if (is.null(format))
+    format <- '%H:%M:%S'
+  handler <- function(x) {
+    strptime(x, format=format)
+  }
+  attr(handler, 'type') <- 'POSIXct'
+  class(handler) <- c('function', 'handler')
+  handler
 }
 
 to_factor <- function(levels=NULL, labels=levels) {
-	handler <- function(x) {
-		if (is.null(levels))
-			factor(x)
-		else
-			factor(x, levels=levels, labels=labels)
-	}
-	class(handler) <- c('function', 'handler')
-	handler
+  handler <- function(x) {
+    if (is.null(levels))
+      factor(x)
+    else
+      factor(x, levels=levels, labels=labels)
+  }
+  attr(handler, 'type') <- 'factor'
+  class(handler) <- c('function', 'handler')
+  handler
 }
 
 to_numeric <- function(dec=0, sign='') {
-	handler <- function(x) {
-		if (is(dec, 'character'))
-			dec <- get(dec, envir=parent.frame())
-		if (! sign %in% c('+', '-', ''))
-			sign <- get(sign, envir=parent.frame())
-		x <- paste0(sign, x)
-		as.numeric(x)/(10^as.numeric(dec))
-	}
-	class(handler) <- c('function', 'handler')
-	handler
+  handler <- function(x) {
+    if (is(dec, 'character'))
+      dec <- get(dec, envir=parent.frame())
+    if (! sign %in% c('+', '-', ''))
+      sign <- get(sign, envir=parent.frame())
+    x <- paste0(sign, x)
+    as.numeric(x)/(10^as.numeric(dec))
+  }
+  attr(handler, 'type') <- 'numeric'
+  class(handler) <- c('function', 'handler')
+  handler
+}
+
+pass_thru_handler <- function() {
+  handler <- identity
+  attr(handler, 'type') <- 'character'
+  class(handler) <- c('function', 'handler')
+  handler
 }
