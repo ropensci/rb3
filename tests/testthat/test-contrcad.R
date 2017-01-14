c0cc467f2da987441aede6963713352469f52774
wilsonfreitas <wilson.freitas@gmail.com>
2017-01-14 12:09:04 -0200

    Updated tests

--- tests/testthat/test-contrcad.R
+++ tests/testthat/test-contrcad.R
@@ -1,6 +1,6 @@
 context('CONTRCAD')
 
 test_that('it should load CONTRCAD with the correct field types', {
-	df <- read_marketdata('../../inst/extdata/CONTRCAD.TXT')
-	expect_is(df$`Indicador de Tipo de Opção`, 'factor')
+  df <- read_marketdata('../../inst/extdata/CONTRCAD.TXT')
+  expect_is(df$tipo_opcao, 'factor')
 })
