e8c9cc961504ce95343ef4e5929c25a7e20ae7b3
wilsonfreitas <wilson.freitas@gmail.com>
2017-01-14 11:16:31 -0200

    Added new features to fields
    fields object now has the *description* attribute, for now it is optional, but that will change soon.
    
    The *pass_thru_handler* is the default handler, replacing *identity*.
    
    `print` method implemented to fields.

--- R/fields.R
+++ R/fields.R
@@ -1,56 +1,90 @@
 
 fields <- function(...) {
-	that <- list(...)
-	class(that) <- 'fields'
-	that
+  that <- list(...)
+  class(that) <- 'fields'
+  that
+}
+
+print.fields <- function(x, ...) {
+  df <- data.frame(
+    `Field name` = fields_names(x),
+    `Description` = fields_description(x),
+    `Width` = fields_widths(x),
+    `Type` = unname(sapply(fields_handlers(x), function(y) attr(y, 'type'))),
+    stringsAsFactors = FALSE,
+    check.names = FALSE
+  )
+  suppressWarnings(
+    print(ascii::ascii(df, include.rownames = TRUE), type = 'org')
+  )
 }
 
 fields_names <- function(x) UseMethod('fields_names', x)
 
 fields_widths <- function(x) UseMethod('fields_widths', x)
 
+fields_description <- function(x) UseMethod('fields_description', x)
+
 fields_handlers <- function(x) UseMethod('fields_handlers', x)
 
 fields_names.fields <- function(fields) {
-	unname(sapply(fields, function(x) as.character(x)))
+  unname(sapply(fields, function(x) as.character(x)))
 }
 
 fields_widths.fields <- function(fields) {
-	unname(sapply(fields, function(x) attr(x, 'width')))
+  unname(sapply(fields, function(x) as.integer(attr(x, 'width'))))
+}
+
+fields_description.fields <- function(fields) {
+  unname(sapply(fields, function(x) attr(x, 'description')))
 }
 
 fields_handlers.fields <- function(fields) {
-	handlers <- lapply(fields, function(x) attr(x, 'handler'))
-	names(handlers) <- fields_names(fields)
-	handlers
+  handlers <- lapply(fields, function(x) attr(x, 'handler'))
+  names(handlers) <- fields_names(fields)
+  handlers
 }
 
-field <- function(name, ...) {
-	parms <- list(...)
-	classes <- sapply(parms, function(x) {
-		if (is(x, 'width'))
-			'width'
-		else if (is(x, 'handler'))
-			'handler'
-		else
-			NULL
-	})
+field <- function(name, description, ...) {
 
-	if (any(classes == 'width'))
-		attr(name, 'width')   <- parms[[ which(classes == 'width')[1] ]]
-	else
-		attr(name, 'width') <- 0
+  if (missing(description)) {
+    attr(name, 'description') <- ''
+    parms <- list(...)
+  } else {
+    if (is(description, 'character')) {
+      attr(name, 'description') <- description
+      parms <- list(...)
+    } else {
+      attr(name, 'description') <- ''
+      parms <- list(description, ...)
+      warning('description invalid type: ', paste(class(description), collapse = ', '))
+    }
+  }
 
-	if (any(classes == 'handler'))
-		attr(name, 'handler')   <- parms[[ which(classes == 'handler')[1] ]]
-	else
-		attr(name, 'handler') <- identity
-	
-	class(name) <- 'field'
-	name
+  classes <- sapply(parms, function(x) {
+    if (is(x, 'width'))
+      'width'
+    else if (is(x, 'handler'))
+      'handler'
+    else
+      NULL
+  })
+
+  if (any(classes == 'width'))
+    attr(name, 'width') <- parms[[ which(classes == 'width')[1] ]]
+  else
+    attr(name, 'width') <- 0
+
+  if (any(classes == 'handler'))
+    attr(name, 'handler') <- parms[[ which(classes == 'handler')[1] ]]
+  else
+    attr(name, 'handler') <- pass_thru_handler()
+
+  class(name) <- 'field'
+  name
 }
 
 print.field <- function(x, ...) {
-	cat(as.character(x), '\n')
+  cat(as.character(x), '\n')
 }
 
