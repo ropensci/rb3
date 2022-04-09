e35b12898c4312a6c39d0c1742adb58a1f29811c
wilsonfreitas <wilson.freitas@gmail.com>
2022-04-09 16:41:07 -0300

    Updated package description

--- R/marketdata.R
+++ R/marketdata.R
@@ -1,16 +1,3 @@
-#' @title Read files from Brazilian Financial Market
-#'
-#' @description
-#' Read the many files used in Brazilian Financial Market and
-#' convert them into useful formats and data structures.
-#'
-#' @name rb3
-#'
-#' @docType package
-#'
-#' @import proto
-#' @import stringr
-NULL
 
 .retrieve_template <- function(filename, template) {
   template <- if (is.null(template)) {
---
+++ R/rb3-package.R
@@ -0,0 +1,13 @@
+#' @title Read files from Brazilian Financial Market
+#'
+#' @description
+#' Read the many files used in Brazilian Financial Market and
+#' convert them into useful formats and data structures.
+#'
+#' @name rb3
+#'
+#' @docType package
+#'
+#' @import proto
+#' @import stringr
+NULL
