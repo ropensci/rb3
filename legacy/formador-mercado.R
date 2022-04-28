756c2d56a7fca234e4e84e8a6477bd8d5d61d13c
wilsonfreitas <wilson.freitas@gmail.com>
2022-04-27 21:31:28 -0300

    New example to select market maker in options series

---
+++ legacy/formador-mercado.R
@@ -0,0 +1,14 @@
+
+ch <- cotahist_get(Sys.Date() - 1, "daily")
+
+ch$HistoricalPrices |>
+  filter(tipo_mercado %in% c(70, 80)) |>
+  distinct(nome_empresa) |>
+  mutate(FM = str_detect(nome_empresa, "\\bFM\\b"))
+
+ch$HistoricalPrices |>
+  filter(tipo_mercado %in% c(70, 80)) |>
+  head()
+
+f <- download_data("COTAHIST_DAILY", refdate = Sys.Date() - 1)
+ch <- read_marketdata(f, "COTAHIST_DAILY", parse_fields = TRUE)
