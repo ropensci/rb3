e6a301be336c537ec1d103b853261b9af727053a
wilsonfreitas <wilson.freitas@gmail.com>
2022-05-27 19:06:34 -0300

    New examples

--- legacy/fatos-relevantes.R
+++ legacy/fatos-relevantes.R
@@ -49,7 +49,7 @@ base64enc::base64decode(hsh) |>
 
 res <- paste0(
   "https://sistemaswebb3-listados.b3.com.br/listedCompaniesProxy/CompanyCall/GetListedSupplementCompany/",
-  list(issuingCompany = "VALE", language = "pt-br") |>
+  list(issuingCompany = "BBAS", language = "pt-br") |>
     jsonlite::toJSON(auto_unbox = TRUE) |>
     charToRaw() |>
     base64enc::base64encode()
@@ -60,16 +60,25 @@ res
 (x <- httr::content(res, as = "text") |> jsonlite::fromJSON())
 x$cashDividends[[1]]
 x$stockDividends[[1]]
+x$subscriptions[[1]]
 x$stockCapital
 x$segment
 x$quotedPerSharSince
+x$commonSharesForm
+x$preferredSharesForm
+x$hasCommom
+x$hasPreferred
 x$code
 x$codeCVM
 x$totalNumberShares
 x$numberCommonShares
 x$numberPreferredShares
-x$subscriptions
+x$roundLot
+x$tradingName
+names(x)
 
+(y <- httr::content(res, as = "text") |> jsonlite::fromJSON(simplifyDataFrame = FALSE))
+View(y)
 
 # ----
 
@@ -81,6 +90,20 @@ base64enc::base64decode(hsh) |>
   rawToChar() |>
   jsonlite::fromJSON()
 
+res <- paste0(
+  "https://sistemaswebb3-listados.b3.com.br/listedCompaniesProxy/CompanyCall/GetDetail/",
+  list(codeCVM = x$codeCVM, language = "pt-br") |>
+    jsonlite::toJSON(auto_unbox = TRUE) |>
+    charToRaw() |>
+    base64enc::base64encode()
+) |> httr::GET(httr::verbose())
+
+res
+
+(g <- httr::content(res, as = "text") |> jsonlite::fromJSON())
+names(g)
+View(g)
+
 # https://sistemaswebb3-listados.b3.com.br/listedCompaniesProxy/CompanyCall/GetListedFinancial/eyJjb2RlQ1ZNIjoiNDE3MCIsImxhbmd1YWdlIjoicHQtYnIifQ==
 
 hsh <- "eyJjb2RlQ1ZNIjoiNDE3MCIsImxhbmd1YWdlIjoicHQtYnIifQ=="
@@ -88,3 +111,57 @@ hsh <- "eyJjb2RlQ1ZNIjoiNDE3MCIsImxhbmd1YWdlIjoicHQtYnIifQ=="
 base64enc::base64decode(hsh) |>
   rawToChar() |>
   jsonlite::fromJSON()
+
+res <- paste0(
+  "https://sistemaswebb3-listados.b3.com.br/listedCompaniesProxy/CompanyCall/GetListedFinancial/",
+  list(codeCVM = x$codeCVM, language = "pt-br") |>
+    jsonlite::toJSON(auto_unbox = TRUE) |>
+    charToRaw() |>
+    base64enc::base64encode()
+) |> httr::GET(httr::verbose())
+
+res
+
+(f <- httr::content(res, as = "text") |> jsonlite::fromJSON())
+names(f)
+f$titleInitial
+f$consolidated |> as_tibble()
+f$unconsolidated |> as_tibble()
+f$freeFloatResult
+
+# https://sistemaswebb3-listados.b3.com.br/listedCompaniesProxy/CompanyCall/GetListedCashDividends/eyJsYW5ndWFnZSI6InB0LWJyIiwicGFnZU51bWJlciI6MSwicGFnZVNpemUiOjIwLCJ0cmFkaW5nTmFtZSI6IlRBRVNBIn0=
+
+hsh <- "eyJsYW5ndWFnZSI6InB0LWJyIiwicGFnZU51bWJlciI6MSwicGFnZVNpemUiOjIwLCJ0cmFkaW5nTmFtZSI6IlRBRVNBIn0="
+
+base64enc::base64decode(hsh) |>
+  rawToChar() |>
+  jsonlite::fromJSON()
+
+res <- paste0(
+  "https://sistemaswebb3-listados.b3.com.br/listedCompaniesProxy/CompanyCall/GetListedCashDividends/",
+  list(
+    tradingName = stringr::str_trim(x$tradingName),
+    language = "pt-br",
+    pageNumber = 1,
+    pageSize = 9999
+  ) |>
+    jsonlite::toJSON(auto_unbox = TRUE) |>
+    charToRaw() |>
+    base64enc::base64encode()
+) |> httr::GET(httr::verbose())
+
+res
+
+d <- httr::content(res, as = "text") |> jsonlite::fromJSON(simplifyDataFrame = FALSE)
+View(d)
+names(d)
+f$titleInitial
+f$consolidated |> as_tibble()
+f$unconsolidated |> as_tibble()
+f$freeFloatResult
+
+
+# https://sistemaswebb3-listados.b3.com.br/listedCompaniesProxy/CompanyCall/GetDetail/eyJjb2RlQ1ZNIjoiMTAyMyIsImxhbmd1YWdlIjoicHQtYnIifQ==
+# https://sistemaswebb3-listados.b3.com.br/listedCompaniesProxy/CompanyCall/GetListedFinancial/eyJjb2RlQ1ZNIjoiNDE3MCIsImxhbmd1YWdlIjoicHQtYnIifQ==
+# https://sistemaswebb3-listados.b3.com.br/listedCompaniesProxy/CompanyCall/GetMaterialFacts/eyJsYW5ndWFnZSI6InB0LWJyIiwiY29kZUNWTSI6IjUwMDI0IiwieWVhciI6MjAxMSwiZGF0ZUluaXRpYWwiOiIyMDExLTAxLTAxIiwiZGF0ZUZpbmFsIjoiMjAxMS0xMi0zMSIsImNhdGVnb3J5Ijo2LCJwYWdlTnVtYmVyIjoxLCJwYWdlU2l6ZSI6NX0="
+# https://sistemaswebb3-listados.b3.com.br/listedCompaniesProxy/CompanyCall/GetListedSupplementCompany/
