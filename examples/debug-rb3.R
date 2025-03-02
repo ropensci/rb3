93a98c0b7c5200e5fa7b96a1682d9f0983e97a47
Wilson Freitas <wilson.freitas@gmail.com>
2025-03-01 22:32:15 -0300

    changed debug script

--- examples/debug-rb3.R
+++ examples/debug-rb3.R
@@ -1,16 +1,21 @@
-devtools::load_all()
+# devtools::load_all()
 
-# debugonce(url_encoded_download)
-single_index_get("IBOV", 1980, cachedir(), TRUE)
+# # debugonce(url_encoded_download)
+# single_index_get("IBOV", 1980, cachedir(), TRUE)
 
-x <- index_get("IBXL", as.Date("1980-01-01"), Sys.Date(), cachedir(), TRUE)
-x <- index_get("IBXX", as.Date("1980-01-01"), Sys.Date(), cachedir(), TRUE)
+# x <- index_get("IBXL", as.Date("1980-01-01"), Sys.Date(), cachedir(), TRUE)
+# x <- index_get("IBXX", as.Date("1980-01-01"), Sys.Date(), cachedir(), TRUE)
 
-library(plotly)
-library(tidyverse)
+# library(plotly)
+# library(tidyverse)
 
-x |>
-  # filter(refdate >= as.Date("2024-01-01")) |>
-  ggplot(aes(x = refdate, y = value)) +
-  geom_line()
-# + scale_y_continuous(trans = "log10")
+# x |>
+#   # filter(refdate >= as.Date("2024-01-01")) |>
+#   ggplot(aes(x = refdate, y = value)) +
+#   geom_line()
+# # + scale_y_continuous(trans = "log10")
+
+f_txt <- system.file("extdata/BD_Arbit.txt", package = "rb3")
+f_txt <- copy_file_to_temp(f_txt)
+
+df <- read_marketdata(f_txt, template = "BD_Arbit")
