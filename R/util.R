11de0e1c40fcea19d4ec79d768faed8f3204fcac
Wilson Freitas <wilson.freitas@gmail.com>
2025-03-08 12:28:22 -0300

    dataset_get has been removed
    This function is no longer necessary. Use template_dataset instead.

--- R/util.R
+++ R/util.R
@@ -48,11 +48,6 @@ dbdir <- function() {
   file.path(cachedir(), "db")
 }
 
-dataset_get <- function(dataset_name) {
-  path <- file.path(dbdir(), dataset_name)
-  arrow::open_dataset(path)
-}
-
 #' cli_progress_along wrapper
 #'
 #' @param x data to iterate through
