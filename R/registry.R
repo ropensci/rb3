1fda9c06b2095848948913e50b15776ef4bffa74
Wilson Freitas <wilson.freitas@gmail.com>
2025-03-15 06:50:02 -0300

    New methods to registry
    New methods $, $<- and length.
    In order to avoid recursive calls in the $ operator the registry attributes have been added as attributes with the attr function.
    It makes this implementation possible.
    The length method has also been implemented.

--- R/registry.R
+++ R/registry.R
@@ -6,11 +6,10 @@ create_registry <- function() {
   create <- function() {
     # Create a new object if no instance exists
     if (is.null(instance)) {
-      new_instance <- list(
-        data = list(),
-        created_at = Sys.time(),
-        .p = environment(create)
-      )
+      new_instance <- list()
+      attr(new_instance, "created_at") <- Sys.time()
+      attr(new_instance, "data") <- list()
+      attr(new_instance, "envir") <- environment(create)
       # Set class for S3 dispatch
       class(new_instance) <- "registry"
       # Store instance in enclosing environment
@@ -20,28 +19,58 @@ create_registry <- function() {
   }
 
   # Return the constructor
-  list(get_instance = create)
+  structure(list(get_instance = create), class = "registry_class")
+}
+
+#' @exportS3Method base::print
+print.registry_class <- function(x, ...) {
+  x <- x$get_instance()
+  print(x)
 }
 
 #' @exportS3Method base::print
 print.registry <- function(x, ...) {
-  cat("registry instance created at:", format(x$created_at), "\n")
-  cat("# elements", length(x$data), "\n")
+  .created <- attr(x, "created_at")
+  cat("registry instance created at:", format(.created), "\n")
+  .data <- attr(x, "data")
+  cat("# elements", length(.data), "\n")
   invisible(x)
 }
 
 registry_get <- function(x, key, ...) {
-  if (exists(key, x$data)) {
-    x$data[[key]]
+  .data <- attr(x, "data")
+  if (key %in% names(.data)) {
+    .data[[key]]
   } else {
     stop(key, " not found in registry")
   }
 }
 
 registry_put <- function(x, key, value, ...) {
-  x$data[[key]] <- value
-  x$.p[["instance"]] <- x
+  .data <- attr(x, "data")
+  .data[[key]] <- value
+  attr(x, "data") <- .data
+  attr(x, "envir")[["instance"]] <- x
   invisible(x)
+}
+
+registry_keys <- function(x, ...) {
+  .data <- attr(x, "data")
+  if (length(.data) == 0) {
+    character(0)
+  } else {
+    names(.data)
+  }
+}
+
+#' @export
+`$.registry` <- function(x, key, ...) {
+  registry_get(x, key)
+}
+
+#' @export
+`$<-.registry` <- function(x, key, value, ...) {
+  registry_put(x, key, value)
 }
 
 #' @export
@@ -59,10 +88,7 @@ names.registry <- function(x, ...) {
   registry_keys(x)
 }
 
-registry_keys <- function(x, ...) {
-  if (length(x$data) == 0) {
-    character(0)
-  } else {
-    names(x$data)
-  }
+#' @exportS3Method base::length
+length.registry <- function(x, ...) {
+  length(attr(x, "data"))
 }
--- tests/testthat/test-registry.R
+++ tests/testthat/test-registry.R
@@ -3,7 +3,7 @@ test_that("it should create a registry class", {
   registry <- create_registry()
   reg <- registry$get_instance()
   expect_s3_class(reg, "registry")
-  expect_true(length(reg$data) == 0)
+  expect_true(length(reg) == 0)
   expect_equal(registry_keys(reg), character(0))
 })
 
@@ -12,12 +12,50 @@ test_that("it should add and retrieve elements", {
   registry <- create_registry()
   reg <- registry$get_instance()
   # Add elements
+  reg <- registry_put(reg, "a", 1)
+  reg <- registry_put(reg, "b", 2)
+  expect_equal(registry_keys(reg), c("a", "b"))
+  expect_equal(registry_get(reg, "a"), 1)
+  expect_equal(registry_get(reg, "b"), 2)
+  # retrieve the instance again
+  expect_equal(registry_keys(reg), c("a", "b"))
+  expect_equal(registry_get(reg, "a"), 1)
+  expect_equal(registry_get(reg, "b"), 2)
+})
+
+test_that("it should add and retrieve elements with [[ operator", {
+  # Create a new registry
+  registry <- create_registry()
+  reg <- registry$get_instance()
+  # Add elements
   reg[["a"]] <- 1
   reg[["b"]] <- 2
   expect_equal(registry_keys(reg), c("a", "b"))
+  expect_equal(names(reg), registry_keys(reg))
+  expect_equal(reg[["a"]], 1)
+  expect_equal(reg[["b"]], 2)
+  expect_equal(registry_get(reg, "a"), reg[["a"]])
+  expect_equal(registry_get(reg, "b"), reg[["b"]])
+  # retrieve the instance again
+  reg <- registry$get_instance()
   expect_equal(names(reg), c("a", "b"))
   expect_equal(reg[["a"]], 1)
   expect_equal(reg[["b"]], 2)
-  expect_equal(registry_get(reg, "a"), 1)
-  expect_equal(registry_get(reg, "b"), 2)
+})
+
+test_that("it should add and retrieve elements with $ operator", {
+  # Create a new registry
+  registry <- create_registry()
+  reg <- registry$get_instance()
+  # Add elements
+  reg$a <- 1
+  reg$b <- 2
+  expect_equal(names(reg), c("a", "b"))
+  expect_equal(reg$a, 1)
+  expect_equal(reg$b, 2)
+  # retrieve the instance again
+  reg <- registry$get_instance()
+  expect_equal(names(reg), c("a", "b"))
+  expect_equal(reg$a, 1)
+  expect_equal(reg$b, 2)
 })
