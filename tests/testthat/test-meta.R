e06d73dc804407110d61efc4c8624e9f6c93d199
Wilson Freitas <wilson.freitas@gmail.com>
2025-03-15 19:57:57 -0300

    Using meta_new to create meta in tests

--- tests/testthat/test-meta.R
+++ tests/testthat/test-meta.R
@@ -1,6 +1,5 @@
 test_that("it should create a new meta object", {
-  template <- template_retrieve("template-test")
-  meta <- template_meta_create(template, var1 = 1, var2 = 2)
+  meta <- meta_new("template-test", var1 = 1, var2 = 2)
 
   expect_equal(meta$template, "template-test")
   args <- list(var1 = 1, var2 = 2) |>
@@ -16,25 +15,29 @@ test_that("it should create a new meta object", {
 })
 
 test_that("it should save meta", {
-  template <- template_retrieve("template-test")
-  meta <- template_meta_create(template, var1 = 1, var2 = 2)
+  meta <- meta_new("template-test", var1 = 1, var2 = 2)
   meta_save(meta)
   meta_file <- meta_file(meta)
   expect_true(file.exists(meta_file))
 })
 
 test_that("it should load existing meta", {
-  template <- template_retrieve("template-test")
-  meta0 <- template_meta_create(template, var1 = 1, var2 = 2)
+  meta0 <- meta_new("template-test", var1 = 1, var2 = 2)
   meta_save(meta0)
 
-  meta1 <- meta_load(template$id, var1 = 1, var2 = 2)
+  meta1 <- meta_load("template-test", var1 = 1, var2 = 2)
   expect_equal(meta0$created, meta1$created)
 })
 
 test_that("it should clean meta", {
-  template <- template_retrieve("template-test")
-  meta <- template_meta_create(template, var1 = 1, var2 = 2)
+  meta <- meta_new("template-test", var1 = 1, var2 = 2)
+  meta_file <- meta_file(meta)
+  meta_clean(meta)
+  expect_false(file.exists(meta_file))
+})
+
+test_that("it should add download to meta", {
+  meta <- meta_new("template-test", var1 = 1, var2 = 2)
   meta_file <- meta_file(meta)
   meta_clean(meta)
   expect_false(file.exists(meta_file))
