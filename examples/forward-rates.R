a1d16dba4578c8f7fd2f28ac71766f5475becf6d
Wilson Freitas <wilson.freitas@gmail.com>
2025-03-15 05:38:55 -0300

    Example updated

--- examples/forward-rates.R
+++ examples/forward-rates.R
@@ -2,17 +2,19 @@
 library(dplyr)
 library(tidyr)
 library(ggplot2)
+library(bizdays)
 
-df <- futures_get("2019-01-01", Sys.Date() - 1)
+df <- futures_get() |>
+  filter(refdate > "2024-01-01", commodity == "DI1") |>
+  collect()
 
 df |>
-  filter(commodity == "DI1") |>
   distinct(symbol) |>
   pull(symbol) |>
   sort()
 
 df_fut <- df |>
-  filter(symbol %in% c("DI1F23", "DI1F28", "DI1F33")) |>
+  filter(symbol %in% c("DI1F26", "DI1F31", "DI1F36")) |>
   mutate(
     maturity_date = maturity2date(maturity_code) |> following("Brazil/ANBIMA"),
     business_days = bizdays(refdate, maturity_date, "Brazil/ANBIMA")
@@ -22,8 +24,8 @@ df_du <- df_fut |>
   select(refdate, symbol, business_days) |>
   pivot_wider(names_from = symbol, values_from = business_days) |>
   mutate(
-    du1 = DI1F28 - DI1F23,
-    du2 = DI1F33 - DI1F28
+    du1 = DI1F31 - DI1F26,
+    du2 = DI1F36 - DI1F31
   ) |>
   select(refdate, du1, du2)
 
@@ -32,12 +34,12 @@ df_fwd <- df_fut |>
   pivot_wider(names_from = symbol, values_from = price) |>
   inner_join(df_du, by = "refdate") |>
   mutate(
-    fwd_F23F28 = (DI1F23 / DI1F28)^(252 / du1) - 1,
-    fwd_F28F33 = (DI1F28 / DI1F33)^(252 / du2) - 1,
-    fwd_F23F33 = (DI1F23 / DI1F33)^(252 / (du2 + du1)) - 1
+    fwd_F26F31 = (DI1F26 / DI1F31)^(252 / du1) - 1,
+    fwd_F31F36 = (DI1F31 / DI1F36)^(252 / du2) - 1,
+    fwd_F26F36 = (DI1F26 / DI1F36)^(252 / (du2 + du1)) - 1
   ) |>
-  select(refdate, fwd_F23F28, fwd_F28F33, fwd_F23F33) |>
-  filter(!is.na(fwd_F23F33)) |>
+  select(refdate, fwd_F26F31, fwd_F31F36, fwd_F26F36) |>
+  filter(!is.na(fwd_F26F36)) |>
   pivot_longer(!refdate, names_to = "tenor", values_to = "forward_rates")
 
 df_fwd |>
@@ -53,7 +55,7 @@ df_fwd |>
 # ----
 
 df_fut <- df |>
-  filter(symbol %in% c("DI1F23", "DI1F33")) |>
+  filter(symbol %in% c("DI1F26", "DI1F36")) |>
   mutate(
     maturity_date = maturity2date(maturity_code) |> following("Brazil/ANBIMA"),
     business_days = bizdays(refdate, maturity_date, "Brazil/ANBIMA")
@@ -63,7 +65,7 @@ df_du <- df_fut |>
   select(refdate, symbol, business_days) |>
   pivot_wider(names_from = symbol, values_from = business_days) |>
   mutate(
-    du = DI1F33 - DI1F23
+    du = DI1F36 - DI1F26
   ) |>
   select(refdate, du)
 
@@ -72,7 +74,7 @@ df_fwd <- df_fut |>
   pivot_wider(names_from = symbol, values_from = price) |>
   inner_join(df_du, by = "refdate") |>
   mutate(
-    fwd = (DI1F23 / DI1F33)^(252 / du) - 1
+    fwd = (DI1F26 / DI1F36)^(252 / du) - 1
   ) |>
   select(refdate, fwd) |> na.omit()
 
@@ -81,6 +83,6 @@ df_fwd |>
   geom_line() +
   labs(
     x = "Data", y = "Taxas a Termo",
-    title = "Histórico de Taxas a Termo 10Y - F23:F33",
+    title = "Histórico de Taxas a Termo 10Y - F26:F36",
     caption = "Fonte B3 - package rb3"
   )
