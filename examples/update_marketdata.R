3a6ea328464ccb4681d82a9c1e286ccaccc2fc93
Wilson Freitas <wilson.freitas@gmail.com>
2025-04-16 06:52:53 -0300

    Update market data fetching: replace daily and outdated references with comprehensive index and historical data calls, ensuring broader data coverage.

--- examples/update_marketdata.R
+++ examples/update_marketdata.R
@@ -1,12 +1,14 @@
 
-fetch_marketdata("b3-cotahist-yearly", year = c(2024, 2023))
+library(rb3)
+library(bizdays)
 
-fetch_marketdata("b3-cotahist-daily", refdate = bizseq("2025-01-01", "2025-03-10", "Brazil/B3"))
-fetch_marketdata("b3-futures-settlement-prices", refdate = bizseq("2018-01-01", "2025-03-10", "Brazil/B3"))
+fetch_marketdata("b3-indexes-composition")
+fetch_marketdata("b3-indexes-current-portfolio", index = indexes_get(), throttle = TRUE)
+fetch_marketdata("b3-indexes-theoretical-portfolio", index = indexes_get(), throttle = TRUE)
+fetch_marketdata("b3-indexes-historical-data", index = indexes_get(), year = 2000:2025, throttle = TRUE)
+fetch_marketdata("b3-cotahist-yearly", year = 2000:2025)
+fetch_marketdata("b3-futures-settlement-prices", refdate = bizseq("2000-01-01", Sys.Date(), "Brazil/B3"))
 fetch_marketdata("b3-reference-rates",
-  refdate = bizseq("2023-01-01", "2025-03-10", "Brazil/B3"),
+  refdate = bizseq("2018-01-01", Sys.Date(), "Brazil/B3"),
   curve_name = c("DIC", "DOC", "PRE")
-)
-fetch_marketdata("b3-bvbg-086",
-  refdate = bizseq("2018-01-01", "2025-03-10", "Brazil/B3")
 )
